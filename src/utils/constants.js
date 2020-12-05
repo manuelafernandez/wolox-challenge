b367b2bf04472a06795f5c721d845bc625e9ede0
manuelafernandez <manuela.dv.fernandez@gmail.com>
2020-12-05 18:00:23 -0300

    Changes in Constans and Styles

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,35 +1,9 @@
-import hour from '../assets/Ic_Hour.svg'
-import homeOffice from '../assets/Ic_HomeOffice.svg'
-import workshops from '../assets/Ic_Workshops.svg'
-import snacks from '../assets/Ic_DrinkSnacks.svg'
-import laptop from '../assets/Ic_laptop.svg'
-import brain from '../assets/Ic_brain.svg'
-
-export const MAX_LENGTH_INPUT = 30
-export const API_BACKEND =
-  'https://private-8e8921-woloxfrontendinverview.apiary-mock.com/'
-
 export const COUNTRIES = [
-  {
-    id: 'ar',
-    country: 'Argentina'
-  },
-  {
-    id: 'ch',
-    country: 'Chile'
-  },
-  {
-    id: 'co',
-    country: 'Colombia'
-  },
-  {
-    id: 'bo',
-    country: 'Bolivia'
-  },
-  {
-    id: 'br',
-    country: 'Brasil'
-  }
+  { id: 'ar', country: 'Argentina' },
+  { id: 'ch', country: 'Chile' },
+  { id: 'co', country: 'Colombia' },
+  { id: 'bo', country: 'México' },
+  { id: 'br', country: 'Perú' }
 ]
 
 export const PROVINCES = {
@@ -37,130 +11,55 @@ export const PROVINCES = {
     id: 'ar',
     country: 'Argentina',
     provinces: [
-      {
-        id: 'ar_ba',
-        province: 'Buenos Aires'
-      },
-      {
-        id: 'ar_co',
-        province: 'Córdoba'
-      },
-      {
-        id: 'ar_sa',
-        province: 'Santa Fe'
-      },
-      {
-        id: 'ar_me',
-        province: 'Mendoza'
-      },
-      {
-        id: 'ar_ca',
-        province: 'Chaco'
-      }
+      { id: 'arg_ba', province: 'Buenos Aires' },
+      { id: 'arg_co', province: 'Córdoba' },
+      { id: 'arg_sf', province: 'Santa Fe' },
+      { id: 'arg_me', province: 'Mendoza' },
+      { id: 'arg_ch', province: 'Chaco' }
     ]
   },
   ch: {
     id: 'ch',
     country: 'Chile',
     provinces: [
-      {
-        id: 'ch_sa',
-        province: 'Santiago de chile'
-      },
-      {
-        id: 'ch_co',
-        province: 'Córdoba'
-      },
-      {
-        id: 'ch_sa',
-        province: 'Santa Fe'
-      },
-      {
-        id: 'ch_me',
-        province: 'Mendoza'
-      },
-      {
-        id: 'ch_ca',
-        province: 'Chaco'
-      }
+      { id: 'ch_ta', province: 'Tarapacá' },
+      { id: 'ch_an', province: 'Antofagasta' },
+      { id: 'ch_at', province: 'Atacama' },
+      { id: 'ch_ay', province: 'Aysén' },
+      { id: 'ch_co', province: 'Coquimbo' }
     ]
   },
   co: {
     id: 'co',
     country: 'Colombia',
     provinces: [
-      {
-        id: 'co_bo',
-        province: 'Bolívar'
-      },
-      {
-        id: 'co_by',
-        province: 'Boyacá'
-      },
-      {
-        id: 'co_ca',
-        province: 'Caldas'
-      },
-      {
-        id: 'co_ca',
-        province: 'Cauca'
-      },
-      {
-        id: 'co_ma',
-        province: 'Magdalena'
-      }
+      { id: 'co_bo', province: 'Bolívar' },
+      { id: 'co_by', province: 'Boyacá' },
+      { id: 'co_ca', province: 'Caldas' },
+      { id: 'co_ca', province: 'Cauca' },
+      { id: 'co_ma', province: 'Magdalena' }
     ]
   },
   bo: {
     id: 'bo',
     country: 'Bolivia',
     provinces: [
-      {
-        id: 'bo_lp',
-        province: 'La Paz'
-      },
-      {
-        id: 'bo_ch',
-        province: 'Chuquisaca'
-      },
-      {
-        id: 'bo_cc',
-        province: 'Cochabamba'
-      },
-      {
-        id: 'bo_sa',
-        province: 'Santa Cruz'
-      },
-      {
-        id: 'bo_ta',
-        province: 'Tarija'
-      }
+      { id: 'bo_lp', province: 'La Paz' },
+      { id: 'bo_ch', province: 'Chuquisaca' },
+      { id: 'bo_co', province: 'Cochabamba' },
+      { id: 'bo_or', province: 'Oruro' },
+      { id: 'bo_po', province: 'Potosí' }
     ]
   },
   br: {
     id: 'br',
     country: 'Brasil',
     provinces: [
-      {
-        id: 'br_am',
-        province: 'Amapá'
-      },
-      {
-        id: 'br_ma',
-        province: 'Mato Grosso'
-      },
-      {
-        id: 'br_mi',
-        province: 'Minas Gerais'
-      },
-      {
-        id: 'br_ri',
-        province: 'Río de Janeiro'
-      },
-      {
-        id: 'br_sa',
-        province: 'São Paulo'
-      }
+      { id: 'br_am', province: 'Amazonas' },
+      { id: 'br_mg', province: 'Minas Gerais' },
+      { id: 'br_pa', province: 'Para' },
+      { id: 'br_rj', province: 'Río de Janeiro' },
+      { id: 'br_sp', province: 'São Paulo' }
     ]
   }
 }
