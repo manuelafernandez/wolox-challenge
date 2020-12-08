9e82efe6b0e6a31433d228a8c47cc9e688b781a3
manuelafernandez <manuela.dv.fernandez@gmail.com>
2020-12-08 16:16:37 -0300

    Changes in redux - reducer/action

--- src/redux/actions/login.js
+++ src/redux/actions/login.js
@@ -1,6 +1,6 @@
 import * as actionTypes from './actionTypes'
 
-export const Login = async (user) => {
+export const Post = async (user) => {
   try {
     const response = await fetch('https://private-8e8921-woloxfrontendinverview.apiary-mock.com/signup', {
       method: 'POST',
@@ -13,7 +13,7 @@ export const Login = async (user) => {
 
 export const loginUser = (user) => async (dispatch, getState) => {
   try {
-    const response = await Login(user)
+    const response = await Post(user)
 
     localStorage.setItem('user', JSON.stringify(user))
     localStorage.setItem('token', response.token)
